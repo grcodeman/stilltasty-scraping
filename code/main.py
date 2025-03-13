c3d3e2a6260637d9053b7537c72bf64d6bd7c6aa
grcodeman <grcodeman@gmail.com>
2025-03-13 19:50:43 -0400

    todo add sections for loop

--- code/main.py
+++ code/main.py
@@ -1,5 +1,9 @@
 # main function be called
 def main():
+    sections = [
+        "https://stilltasty.com/searchitems/index/26", #fruit
+        "https://stilltasty.com/searchitems/index/25" #vegetables
+    ]
     return 0
 
 # clean launch
